7458e30a3e2cdd23552667ba7b8a080b8d8917d7
Valeria Kirillina <violentyyyouth@gmail.com>
2017-04-02 20:11:22 +0300

    Ошибка

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 "use strict";
 
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del");
+var run = require("run-sequence");
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png, jpg, gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+      ]))
+    .pipe(gulp.dest("img"));
+});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -15,9 +30,15 @@ gulp.task("style", function() {
       autoprefixer({browsers: [
         "last 2 versions"
       ]})
+      mqpacker({
+        sort: true
+      })
     ]))
     .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -31,4 +52,24 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("build", function(fn) {
+  run("style", "images", fn);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+      "fonts/**/*.{woff,woff2}",
+      "img/**",
+      "js/**",
+      "*.html"
+    ], {
+      base: "."
+})
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
 });
